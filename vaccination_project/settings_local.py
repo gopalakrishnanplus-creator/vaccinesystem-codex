7f95bb719662b9b34f4dfa98fc673c2ac3a63519
Inditech-tech <inditech-tech@Inditech-techs-Mac-mini.local>
2026-04-22 09:34:51 +0530

    Fix relative static URL paths

--- vaccination_project/settings_local.py
+++ vaccination_project/settings_local.py
@@ -82,7 +82,7 @@ TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
